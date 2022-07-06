3716220c87cc644282e8f6e106d72f4f765396bc
Samuel Wong <35249407+desktopofsamuel@users.noreply.github.com>
2022-07-06 17:46:49 +0800

    add remark & rehype & chakra

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import "../styles/prism.css";
+import "@fontsource/roboto-serif/400.css"
+import "@fontsource/roboto-serif/variable.css"
+import customTheme from "../theme.js";
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider resetCSS={true} theme={customTheme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ theme.js
@@ -0,0 +1,330 @@
+import { extendTheme } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
+
+// import Heading from '@/components/element/heading';
+
+const customTheme = extendTheme({
+  config: {
+    cssVarPrefix: "dos",
+    initialColorMode: "light",
+    useSystemColorMode: false,
+  },
+  fonts: {
+    body: "Roboto Serif, Noto Sans HK, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen-Sans, Ubuntu, Cantarell, Helvetica Neue, sans-serif",
+    heading:
+      "Roboto Serif, Noto Sans HK, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen-Sans, Ubuntu, Cantarell, Helvetica Neue, sans-serif",
+    mono: "Space Mono, IBM Plex Mono, monospace",
+  },
+  colors: {
+    transparent: "rgba(0,0,0,0)",
+    text: {
+      100: "lightgray",
+      400: "darkgray",
+    },
+    brand: {
+      100: "orange",
+      400: "red",
+    },
+    primary: {
+      300: "#33aaff",
+      400: "#78c7ff",
+      500: "#0077CC",
+    },
+    secondary: {
+      300: "rgb(202,20,20,0.7)",
+      400: "#ca1414",
+    },
+    indigo: {
+      100: "#EAF4FA",
+      200: "#D6E8F6",
+      300: "#BBBFCC",
+      400: "#9AB2CD",
+      500: "#748cad",
+      600: "#546D94",
+      700: "#3A507C",
+      800: "#253764",
+      900: "#1A2025",
+    },
+  },
+  semanticTokens: {
+    colors: {
+      text: {
+        default: "gray.600",
+        _dark: "indigo.300",
+      },
+      background: {
+        default: "white",
+        _dark: "indigo.900",
+      },
+      border: {
+        default: "indigo.300",
+        _dark: "indigo.800",
+      },
+      outline: {
+        default: "gray.200",
+        _dark: "gray.700",
+      },
+    },
+  },
+  fontSizes: {
+    xs: "0.75rem",
+    sm: "0.875rem",
+    md: "1rem",
+    lg: "1.125rem",
+    xl: "1.25rem",
+    "2xl": "1.5rem",
+    "3xl": "1.875rem",
+    "4xl": "2.25rem",
+    "5xl": "3rem",
+    "6xl": "3.75rem",
+    "7xl": "4.5rem",
+    "8xl": "6rem",
+    "9xl": "8rem",
+  },
+  fontWeights: {
+    hairline: 100,
+    thin: 200,
+    light: 300,
+    normal: 400,
+    medium: 500,
+    semibold: 600,
+    bold: 700,
+    extrabold: 800,
+    black: 900,
+  },
+  lineHeights: {
+    normal: "normal",
+    none: 1,
+    shorter: 1.25,
+    short: 1.375,
+    base: 1.5,
+    tall: 1.625,
+    taller: "2",
+    3: ".75rem",
+    4: "1rem",
+    5: "1.25rem",
+    6: "1.5rem",
+    7: "1.75rem",
+    8: "2rem",
+    9: "2.25rem",
+    10: "2.5rem",
+  },
+  letterSpacings: {
+    tighter: "-0.05em",
+    tight: "-0.025em",
+    normal: "0",
+    wide: "0.025em",
+    wider: "0.05em",
+    widest: "0.1em",
+  },
+  components: {
+    Button: {
+      baseStyle: (props) => ({
+        // background: mode('indigo.100', 'indigo.900')(props),
+        _hover: {
+          textDecoration: "none",
+        },
+        // _disabled: {
+        //   background: mode('primary.500', 'primary.400')(props),
+        //   _hover: {
+        //     backgroundColor: mode('primary.500', 'primary.400')(props),
+        //   },
+        // },
+      }),
+      variants: {
+        solid: (props) => ({
+          color: mode("primary.500", "primary.400")(props),
+          _hover: {
+            // backgroundColor: 'primary.500',
+            // color: 'white',
+            textDecoration: "none",
+          },
+        }),
+        ghost: (props) => ({
+          background: "none",
+          color: mode("primary.500", "white")(props),
+          _hover: {
+            background: mode("indigo.100", "whiteAlpha.200")(props),
+            color: mode("primary.500", "white")(props),
+            textDecoration: "none",
+          },
+        }),
+        outline: (props) => ({
+          borderColor: "indigo.200",
+        }),
+        brand: (props) => ({
+          backgroundColor: mode("primary.500", "primary.400")(props),
+
+          color: "white",
+        }),
+        disabled: (props) => ({
+          color: "primary.500",
+        }),
+        defaultProps: {
+          // colorScheme: 'indigo',
+        },
+      },
+    },
+    Text: {
+      baseStyle: (props) => ({
+        transition: "all 100ms ease-in-out",
+        // color: mode('red.900', 'whiteAlpha.100')(props),
+      }),
+      variants: {
+        small: (props) => ({
+          margin: "0",
+          marginBottom: "2",
+          fontSize: "sm",
+        }),
+      },
+    },
+    Link: {
+      baseStyle: (props) => ({
+        transition: "all 500ms ease-in-out",
+        borderBottomColor: "transparent",
+        color: mode("gray.800", "white.300"),
+        textDecoration: "none",
+        _hover: {
+          textDecoration: "none",
+        },
+      }),
+      variants: {
+        noeffect: (props) => ({
+          _hover: {
+            borderBottomColor: "transparent",
+          },
+        }),
+        postTitle: (props) => ({
+          paddingBottom: "2px",
+          borderBottom: "1.5px dotted rgba(0,0,0,0)",
+          transition: "all 0.1s ease-in-out",
+          display: "inline",
+          _hover: {
+            textDecoration: "none",
+            borderBottom: "1.5px dotted darkgrey",
+          },
+        }),
+        navigation: (props) => ({
+          borderBottomWidth: "1px",
+          borderBottomColor: "transparent",
+          _hover: {
+            color: mode("primary.500", "primary.400"),
+            borderBottomColor: mode("primary.500", "primary.400"),
+            borderBottomWidth: "1px",
+            textDecoration: "none",
+          },
+        }),
+      },
+    },
+    VStack: {
+      baseStyle: (props) => ({
+        align: "flex-start",
+        spacing: "8",
+      }),
+    },
+    Heading: {
+      baseStyle: {
+        fontSize: "2xl",
+      },
+      variants: {
+        pagetitle: {
+          fontSize: "4xl",
+          mt: "0",
+          mb: "8",
+        },
+        title: {
+          fontSize: "2xl",
+          my: "0",
+          lineHeight: "tall",
+        },
+        heading: {
+          fontSize: "3xl",
+        },
+      },
+      defaultProps: {
+        variant: "pagetitle",
+      },
+    },
+  },
+  styles: {
+    global: (props) => ({
+      body: {
+        lineHeight: "taller",
+        letterSpacing: "tight",
+        background: "background",
+      },
+      p: {
+        color: "text",
+        my: "4",
+        a: {
+          color: mode("primary.500", "primary.400")(props),
+          borderBottomWidth: "1px",
+          borderBottomColor: mode("primary.500", "primary.400")(props),
+        },
+      },
+      a: {
+        // color: mode("primary.500", "primary.400")(props),
+        // _hover: {
+        //   borderBottomWidth: '1px',
+        //   borderBottomColor: 'primary.500',
+        //   borderBottomStyle: 'solid',
+        // },
+        borderBottomWidth: "1px",
+        borderBottomColor: "transparent",
+        _hover: {
+          color: mode("primary.500", "primary.400")(props),
+          borderBottomColor: mode("primary.500", "primary.400")(props),
+          borderBottomWidth: "1px",
+        },
+      },
+      h1: {
+        fontSize: "3xl",
+        fontWeight: "bold",
+        mt: "8",
+        mb: "4",
+      },
+      h2: {
+        fontSize: "2xl",
+        fontWeight: "bold",
+        lineHeight: "taller",
+        mt: "8",
+        mb: "4",
+      },
+      h3: {
+        fontSize: "md",
+        fontWeight: "bold",
+        mt: "8",
+        mb: "4",
+      },
+      ul: {
+        listStyle: "square",
+        my: "2",
+      },
+      li: {
+        color: mode("gray.600", "indigo.300")(props),
+      },
+      pre: {
+        my: "2",
+        whiteSpace: "pre-wrap",
+        fontSize: "sm",
+        lineHeight: "short",
+        backgroundColor: "gray.200",
+      },
+      code: {
+        padding: "1",
+        margin: "2 0",
+        backgroundColor: "gray.200",
+        borderRadius: "2",
+      },
+      iframe: {
+        py: "8",
+      },
+      blockquote: {
+        fontSize: "lg",
+        fontStyle: "italic",
+      },
+    }),
+  },
+});
+
+export default customTheme;
