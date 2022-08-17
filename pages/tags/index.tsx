ebd333fff1030aab1fd4f220962cc62be2484809
Samuel Wong <35249407+desktopofsamuel@users.noreply.github.com>
2022-08-17 14:41:28 +0800

    fix: rMove allTags to StaticProps

--- pages/tags/index.tsx
+++ pages/tags/index.tsx
@@ -1,13 +1,24 @@
 import Layout from "@/components/Layout";
 import { allTags } from "@/lib/content";
 import { Heading, Text } from "@chakra-ui/react";
+import type { GetStaticProps, InferGetStaticPropsType } from "next";
 import Link from "next/link";
 
-export default function TagListPage() {
+export const getStaticProps: GetStaticProps<{
+  tags: any
+}> = ({ params }: any) => {
+  return {
+    props: {
+      tags: allTags,
+    },
+  };
+};
+
+export default function TagListPage({ tags }:any ) {
   return (
     <Layout>
       <Heading variant="pagetitle">Tags</Heading>
-      {allTags.map((tag, i) => (
+      {tags.map((tag: { path: string, name: string }, i: number) => (
         <Link href={tag.path} key={i}>
           <Text>{tag.name}</Text>
         </Link>
