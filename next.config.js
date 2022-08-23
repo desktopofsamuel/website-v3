5d2d933ddc92cdf9f7a5923e8c4bfe71c87c833a
Samuel Wong <35249407+desktopofsamuel@users.noreply.github.com>
2022-08-23 08:18:39 +0800

    photo: add nyc

--- next.config.js
+++ next.config.js
@@ -8,10 +8,6 @@ module.exports = withContentlayer({
   images: {
     domains: ["i.scdn.co", "a.ltrbxd.com"],
   },
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
   experimental: {
     images: {
       allowFutureImage: true,
