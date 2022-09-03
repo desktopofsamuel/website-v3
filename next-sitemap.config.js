09c967065dd97a16df1b73340c10f6d782b2e013
Samuel Wong <35249407+desktopofsamuel@users.noreply.github.com>
2022-09-03 22:22:06 +0800

    chore: update sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://desktopofsamuel-website-v3.vercel.app/',
+  siteUrl: process.env.SITE_URL || 'https://desktopofsamuel.com/',
   generateRobotsTxt: true, // (optional)
   // ...other options
 }
